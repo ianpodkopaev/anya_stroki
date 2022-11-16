133bf8ad31b952acbdd6a3530a41c6837dd04353
ianpodkopaev <ian.podkopaev@gmail.com>
2022-11-16 13:43:40 +0300

    Add project files.

---
+++ anya_stroki/anya_stroki.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main() {
+    string str, wrd, tmp;
+    cout << "String: ";
+    getline(std::cin, str);
+    cout << "Word: ";
+    cin >> wrd;
+
+    istringstream ss(str);
+    size_t cnt = 0;
+    while (ss >> tmp)
+        if (tmp == wrd)
+            ++cnt;
+    std::cout << "meet " << cnt << " times." <<endl;
+
+    return 0;
+}
